e094bbfecee9be73073f5039d55daf4aacf23906
Hossein913 <Hossein913@gmail.com>
2023-10-05 11:05:35 +0330

    Repositories Added

---
+++ App.Infra.Data.Repos.EF/Category.cs
@@ -0,0 +1,14 @@
+﻿using App.Domain.core.Datas.EfRipository;
+using App.Domain.core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.Data.Repos.EF
+{
+    public class Category : ICategory
+    {
+    }
+}
---
+++ App.Infra.Data.Repos.EF/OrderStatus.cs
@@ -0,0 +1,13 @@
+﻿using App.Domain.core.Datas.EfRipository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.Data.Repos.EF
+{
+    public class OrderStatus : IOrderStatus
+    {
+    }
+}
